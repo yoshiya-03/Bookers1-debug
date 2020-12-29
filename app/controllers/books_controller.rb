afa193604a89cfef2813a5e0c6fb6c5d92dd7ad8
yoshiya-03 <yoshiya03y07@gmail.com>
2020-12-29 08:05:53 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :hoge, only: [:show, :edit, :update, :destroy]
 
   def index
     @book = Book.new
@@ -37,10 +37,9 @@ class BooksController < ApplicationController
 
   private
    
-    def set_book
+    def hoge
       @book = Book.find(params[:id])
     end
-
 
     def book_params
       params.require(:book).permit(:title, :body)
